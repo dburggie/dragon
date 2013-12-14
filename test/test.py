090bb929f1a87154f79e9ce376ffb5572d3e1bf9
Duncan Burggraf <dcburggraf@gmail.com>
2013-12-14 14:42:23 -0800

    readme and tests

--- test/test.py
+++ test/test.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python2
 from dragon import Dragon
 
-of = "test-03.png"
+of = "test-05.png"
 
-d = Dragon(3)
+d = Dragon(1)
 
-for i in range(14):
+for i in range(19):
     print "\n### ROUND {} ###\n".format(i)
     d.iterate()
 
